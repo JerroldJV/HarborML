54013a794450007e5998064a453698719db91a54
JerroldJV <jerroldjvincent@gmail.com>
2019-06-07 11:23:27 -0700

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='harborml',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(exclude=['tests*']),
     license='GPLv3',
     description='Framework for building, training, and deploying machine learning and AI solutions via containers',
